aef35933331c4a85b996550b5ca426072d17acb0
Thomas Orozco <thomas@orozco.fr>
2016-03-01 21:37:01 +0100

    9.4-contrib: include mysql_fdw

--- 9.4-contrib/install-extras.sh
+++ 9.4-contrib/install-extras.sh
@@ -16,6 +16,7 @@ DEPS=(
   libpq-dev "^postgresql-server-dev-${PG_VERSION}$"
   freetds-dev
   libv8-3.14-dev
+  libmysqlclient-dev
 )
 
 apt-install "${DEPS[@]}"
@@ -24,3 +25,4 @@ pip install pgxnclient
 pgxn install "tds_fdw==1.0.7"
 pgxn install "plv8==1.4.4"
 pgxn install --testing "pg_proctab==0.0.5"
+USE_PGXS=1 pgxn install "mysql_fdw==2.1.2"
