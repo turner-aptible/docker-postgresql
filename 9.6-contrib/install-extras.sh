3a6d5b09dfa5e27ab9c427d0b7c34026adaf6ce1
Alex Kubacki <alex@aptible.com>
2017-03-19 10:47:01 -0400

    Add 9.6-contrib image
    mysql_fdw "removed" in 9.6-contrib:
     supported on 9.6 with 2.0.2 version : https://github.com/EnterpriseDB/mysql_fdw/releases/tag/REL-2_2_0
     but that version not available yet on pgxn :  https://pgxn.org/dist/mysql_fdw/
    
    tds_fdw "removed" in 9.6-contrib:
     supported on 9.6 with 1.0.8 version : https://github.com/tds-fdw/tds_fdw/releases/tag/v1.0.8
     but that version is not available yet on pgxn : https://pgxn.org/dist/tds_fdw/

---
+++ 9.6-contrib/install-extras.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+
+# Install packaged extensions first
+apt-install "^postgresql-plpython-${PG_VERSION}$" "^postgresql-plpython3-${PG_VERSION}$"
+
+# Now, install source extensions
+
+# We'll need backports for libv8
+echo "deb http://httpredir.debian.org/debian wheezy-backports main" >> /etc/apt/sources.list
+
+DEPS=(
+  build-essential python-pip
+  libpq-dev "^postgresql-server-dev-${PG_VERSION}$"
+  libv8-3.14-dev
+  python-dev
+)
+
+apt-install "${DEPS[@]}"
+pip install pgxnclient
+
+pgxn install "plv8==1.4.4"
+pgxn install "multicorn==1.3.3"
