8ecf5452ecbd748d31846e042d3ee7ffedead8e8
Bayley Adams <bayley.adams@aptible.com>
2020-03-27 17:59:46 -0700

    add 3 extensions

--- 12-contrib/install-extras.sh
+++ 12-contrib/install-extras.sh
@@ -4,4 +4,5 @@ set -o nounset
 set -x
 
 # Install packaged extensions first
-apt-install "^postgresql-plperl-${PG_VERSION}$"
+apt-install "^postgresql-plperl-${PG_VERSION}$" "^pgagent$" \
+  "^postgresql-plpython3-${PG_VERSION}$" "^postgresql-${PG_VERSION}-wal2json$"
