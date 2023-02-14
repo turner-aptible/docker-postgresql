a63823d079258f4f09455e6980c2198fab5ab6d9
Alex Kubacki <usernotfound@gmail.com>
2023-02-14 12:48:52 -0700

    Slap pg_partman into 13-contrib

--- 13-contrib/install-extras.sh
+++ 13-contrib/install-extras.sh
@@ -12,3 +12,38 @@ apt-key add /tmp/GPGkeys/pglogical.key
 # Install packaged extensions first
 apt-install "^postgresql-${PG_VERSION}-pglogical$" "^pgagent$" "^postgresql-${PG_VERSION}-pgaudit$" \
             "^postgresql-${PG_VERSION}-repack$"
+
+DEPS=(
+  build-essential
+  "^postgresql-server-dev-${PG_VERSION}$"
+)
+
+apt-install "${DEPS[@]}"
+
+# Install extensions from source (expects tarball URL as argument)
+install_extension_from_source() {
+  tarball_url=$1
+  shift
+  shasum=$1
+  shift
+
+  pushd .
+  tempdir=$(mktemp -d)
+
+  cd $tempdir
+  wget -O extension.tar.gz $tarball_url
+  echo "${shasum}  extension.tar.gz" | sha1sum -c - || exit
+  mkdir -p extension
+  tar xzf extension.tar.gz -C extension --strip-components 1
+
+  cd extension
+  make USE_PGXS=1 install
+
+  cd $tempdir
+  rm -rf extension extension.tar.gz
+  popd
+}
+
+install_extension_from_source \
+  https://github.com/pgpartman/pg_partman/archive/v4.3.1.tar.gz \
+  22eb8069800614a4601a4ce76519a3d9a41c3311
