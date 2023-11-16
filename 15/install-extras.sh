f8f6f5ee84e08ec5b8fc5605cc4626a461cfc609
Michael Wang <wang.michael213@gmail.com>
2023-11-16 09:01:15 -0500

    minor updates

--- 15/install-extras.sh
+++ 15/install-extras.sh
@@ -4,7 +4,7 @@ set -o nounset
 set -x
 
 # We'll need the pglogical repo...
-echo "deb [arch=amd64] https://dl.2ndquadrant.com/default/release/apt stretch-2ndquadrant main" >> /etc/apt/sources.list
+echo "deb [arch=amd64] https://dl.2ndquadrant.com/default/release/apt bullseye-2ndquadrant main" >> /etc/apt/sources.list
 
 # ...and its key
 apt-key add /tmp/GPGkeys/pglogical.key
